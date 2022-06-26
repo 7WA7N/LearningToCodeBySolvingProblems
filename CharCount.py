7755a31ca5030ebde077bd045ef5daf5479fbf6d
Ali Awan <79001319+7WA7N@users.noreply.github.com>
2022-06-27 00:16:55 +0500

    added CharCount instead using .count() method

---
+++ CharCount.py
@@ -0,0 +1,5 @@
+words = input('Enter your string: ')
+char = input('Enter character you want to count: ')
+charcount = words.count(char)
+print(charcount)
+
--- WordCount.py
+++
@@ -1,7 +0,0 @@
-def word_count(words):
-    total_words = words.count(' ') + 1
-    return print(f'Total Words are {total_words}')
-
-word_count('Hello World')
-word_count('Hello World Hello World')
-word_count('Hello World Hello World Hello World')
